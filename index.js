1065fc2ec3a557e4ebc00f5975308baf846df434
Fabrice <122691554+fabriceCODER@users.noreply.github.com>
2023-06-01 21:40:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from "./Hero";
+import Navbar from "./NavBar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Feedbacks from "./FeedBacks";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  CanvasLoader,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+};
